df6868307373fd2cbc2c3e1047ee7bdf5ccf34f7
rayzeydmusee <dmusee721@gmail.com>
2022-07-10 17:09:00 +0300

    sidebar commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,16 +6,16 @@ import { BellIcon,DotsHorizontalIcon, BookmarkIcon, ClipboardIcon, DotsCircleHor
 import 'tailwindcss/tailwind.css'
 function Sidebar() {
   return (
-    <div>
+    <div className='hidden sm:flex flex-col p-2 xl:items-start fixed h-full'>
         {/*Twitterlogo*/}
-        <div ClassName="hoverOver">
+        <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
       <Image width="50" height="50" src="https://imgs.search.brave.com/HSqZIViVT05nuvKYi1zxI4wa9U4S0cYVgXJBDNUjowc/rs:fit:1200:1200:1/g:ce/aHR0cDovLzEwMDBs/b2dvcy5uZXQvd3At/Y29udGVudC91cGxv/YWRzLzIwMTcvMDYv/VHdpdHRlci1Mb2dv/LnBuZw">
         
       </Image>
          </div>
     {/*menu*/}
-     <div ClassName="">
-        <SidebarMenuItem text="Home" Icon={HomeIcon}/>
+     <div className="mt-4 mb-2.5 xl:items-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active/>
         <SidebarMenuItem text="Explore" Icon={HashtagIcon}/>
         <SidebarMenuItem text="Notification" Icon={BellIcon}/>
         <SidebarMenuItem text="Messages" Icon={InboxInIcon}/>
@@ -26,15 +26,19 @@ function Sidebar() {
         
         </div>
      {/*button*/}
-        <button>Tweet</button>
+        <button className='bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline '>Tweet</button>
          {/*min9i-prifile*/}
-         <div  ClassName="h-5" >
-        <img height="50px" src='https://th.bing.com/th/id/R.1a522e523f8662b44bfc6f16bd9dd86a?rik=KXam80wdAmdujA&pid=ImgRaw&r=0'/>
-        <div ClassName="">
-            <h4>Dan Musee</h4>
-            <p>@rayzrmd</p>
+       <div  className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto" >
+        <img className='h-10 w-10 rounded-full xl:mr-2'
+       
+         src='https://imgs.search.brave.com/sQF-4cufXFk2wnJpcrER7MdgLKPTHL-oTdLW9AAqHHM/rs:fit:600:600:1/g:ce/aHR0cHM6Ly8xLmJw/LmJsb2dzcG90LmNv/bS8tQWxzcTlCbnY2/N2svVkE0T25aazEz/Q0kvQUFBQUFBQUFE/dWcvV2FDb25VUlVn/OE0vczE2MDAvUm91/bmQlMkJQcm9maWxl/JTJCUGljLnBuZw'
+         alt="user-img"
+        />
+        <div className="leading-5 hidden xl:inline ">
+            <h4 className="font-bold">Dan Musee</h4>
+            <p className='text-gray-500'>@rayzrmd</p>
         </div>
-        <DotsHorizontalIcon height="50px" ClassName='h-5'/>
+        <DotsHorizontalIcon  className='h-5 xl:ml-8 hidden xl:inline'/>
          </div>
          </div>
   
--- components/SidebarMenuItem.js
+++ components/SidebarMenuItem.js
@@ -1,10 +1,10 @@
 import 'tailwindcss/tailwind.css'
 
-function SidebarMenuItem({text,Icon}) {
+function SidebarMenuItem({text,Icon, active}) {
   return (
-    <div>
-        <Icon  ClassName="hoverEffect" height="25px"/>
-        <span>{text}</span>
+    <div className="hoverEffect flex items-center text-gray-700 justify-center xl:justify-start text-lg space-x-3">
+        <Icon className="h-5" />
+        <span className={`${active && "font-bold"} hidden xl:inline`}>{text}</span>
     
     </div>
   )
